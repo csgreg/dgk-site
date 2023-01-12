97ba498ea10fa11e75bbe0ba1df46932bc4cf760
Gergo Csizmadia <gergo.csizmadia@lensa.com>
2023-01-12 17:20:18 +0100

    sitemap v3

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,18 +1,12 @@
-import { json } from '@sveltejs/kit';
-
 export async function GET() {
-	const headers = {
-		'Cache-Control': 'max-age=0, s-maxage=3600',
-		'Content-Type': 'application/xml'
-	};
-	return json({
-		headers,
-		body: `<?xml version="1.0" encoding="UTF-8" ?>
+	return new Response(
+		`
+    <?xml version="1.0" encoding="UTF-8" ?>
     <urlset
       xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
       xmlns:xhtml="https://www.w3.org/1999/xhtml"
       xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+      xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
       xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
       xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
     >
@@ -21,6 +15,31 @@ export async function GET() {
         <changefreq>daily</changefreq>
         <priority>0.9</priority>
       </url>
-    </urlset>`
-	});
+      <url>
+        <loc>https://www.photographydgk.hu/about</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+      </url>
+      <url>
+        <loc>https://www.photographydgk.hu/contact</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.5</priority>
+      </url>
+      <url>
+        <loc>https://www.photographydgk.hu/gallery</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+      </url>
+      <url>
+        <loc>https://www.photographydgk.hu/movies</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+      </url>
+    </urlset>`.trim(),
+		{
+			headers: {
+				'Content-Type': 'application/xml'
+			}
+		}
+	);
 }
